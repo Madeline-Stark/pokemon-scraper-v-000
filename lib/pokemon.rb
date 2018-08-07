2bd46abf19b448c6c66c33bbdba76146772df417
Madeline Stark <madeline.stark1@gmail.com>
2018-08-07 22:27:03 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,12 +17,10 @@ class Pokemon
  end
 
  def self.find(id, db)
-   found = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+   found = db.execute("SELECT * FROM pokemon WHERE id = ?", id) #can't interpolate in sql 
    lost = []
    lost = found.flatten
    Pokemon.new(id: lost[0], name: lost[1], type: lost[2], db: db) #need to be key value pairs
  end
- # what we are going want to do is create a sql query by id and
- # store that array that we get as a result of the query into a variable
 
 end
